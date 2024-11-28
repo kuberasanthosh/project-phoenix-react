a68202ff48059a808f1c1fe8256a074b9d993cf7
Kubera Santhosh Venkateswara Gupta <kvenkateswaragu3722@conestogac.on.ca>
2024-11-27 20:24:06 -0700

    Inclusion - Hamburger + NavLink + Layout changes with css

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,21 +1,40 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
+import {Twirl as Hamburger} from 'hamburger-react';
+import { useState } from "react";
 
 const Layout = () => {
+  const [isActive, setActive] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
+  const linkToggleClass = (e:React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    toggleClass();
+    debugger;
+    let path : any = e.currentTarget.attributes?.getNamedItem('href')?.nodeValue != null 
+      ? e.currentTarget.attributes?.getNamedItem('href')?.nodeValue : 
+      "/";
+    navigate(path);
+  }
   return (
     <>
     <header>
+      <Hamburger onToggle={toggleClass} />
       <p className="app-title">Phoenix React App</p>
     </header>
-      <nav>
+      <nav className={ isActive ? "nav-menu mobile-menu" : "nav-menu" }>
         <ul className="nav-menu-items">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink to="/" onClick={(e) => linkToggleClass(e)}>Home</NavLink>
           </li>
           <li>
-            <Link to="/buttontypes">Button Types</Link>
+            <NavLink to="/buttontypes" onClick={(e) => linkToggleClass(e)}>Button Types</NavLink>
           </li>
           <li>
-            <Link to="/counter">Counter</Link>
+            <NavLink to="/counter" onClick={(e) => linkToggleClass(e)}>Counter</NavLink>
           </li>
         </ul>
       </nav>
